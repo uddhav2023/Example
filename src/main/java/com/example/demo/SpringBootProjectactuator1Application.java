811fde10f27180c559ff1929251fb55338f8ec3e
uddhav2023 <uddhavbhandarge@gmail.com>
2023-06-15 08:46:08 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringBootProjectactuator1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectactuator1Application {
+
+	public static void main(String[] args) {
+		
+		System.out.println("springboot actuator");
+		SpringApplication.run(SpringBootProjectactuator1Application.class, args);
+	}
+
+}
